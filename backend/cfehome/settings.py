108a2147c1378f4ca097e671d856c2545c2d3cc9
Lehan Ouyang <lo739@nyu.edu>
2022-11-25 15:36:09 -0500

    Configure settings to include postgresql db

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testDB',
+        'USER': 'postgres',
+        'PASSWORD': 'Ouyang0328',
+        'HOST': 'database-2.cok6eq7jasfe.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
